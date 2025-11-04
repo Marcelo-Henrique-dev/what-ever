b50d79520891e5ec0b198d055a11ffaffec2850e
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2025-11-04 19:27:52 -0300

    front start

--- src/main/resources/static/front/src/app/app.routes.ts
+++ src/main/resources/static/front/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { PlayerComponent } from './components/pages/player/player.component';
+import { QuizComponent } from './components/pages/quiz/quiz.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "/home", pathMatch: 'full'},
+    {path: "home", component: PrincipalComponent, children:[
+        {path: "welcome", component: WelcomeComponent},
+        {path: "player", component: PlayerComponent},
+        {path: "quiz", component: QuizComponent}
+    ]}
+];
---
+++ src/main/resources/static/front/src/app/components/layout/playerlist/playerlist.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playerlist',
+  imports: [],
+  templateUrl: './playerlist.component.html',
+  styleUrl: './playerlist.component.css'
+})
+export class PlayerlistComponent {
+
+}
