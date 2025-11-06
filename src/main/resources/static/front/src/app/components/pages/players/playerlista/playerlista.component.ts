b2f752d55d0d23f43f2e932afc48c24e1688552b
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2025-11-06 10:54:17 -0300

    paginação concluída

--- src/main/resources/static/front/src/app/components/pages/players/playerlista/playerlista.component.ts
+++ src/main/resources/static/front/src/app/components/pages/players/playerlista/playerlista.component.ts
@@ -22,6 +22,9 @@ export class PlayerlistaComponent {
   firstLoad: boolean = true;
   pages: number[] = [];
   pageNumber!:number;
+  startPage: number = 0;
+  readonly maxPagesToShow = 6;
+  readonly bufferPages = 3;
 
   constructor(){
     this.listPlayers();
@@ -32,9 +35,12 @@ export class PlayerlistaComponent {
       next: (response) => {
         this.lista = response.content;
         this.pageNumber = response.number;
-        for(let i=0; i<response.totalPages; i++){
-          this.pages.push(i); 
+        if(this.firstLoad && this.pages.length === 0){
+          for(let i = 0; i<response.totalPages; i++){
+            this.pages.push(i);
+          }
         }
+        this.adjustStartPage();
         this.isLoading = false;
         if(this.page == 0 && this.firstLoad){
           Swal.fire({
@@ -60,14 +66,53 @@ export class PlayerlistaComponent {
   }
 
   avancarPagina(){
-    this.page++;
-    this.listPlayers();
-    this.firstLoad = false;
+    if(this.page == this.pages.length-1){
+      return;
+    }else{
+      this.page++;
+      this.listPlayers();
+      this.firstLoad = false;
+    }
   }
   
   voltarPagina(){
-    this.page--;
-    this.listPlayers();
+    if(this.page == 0){
+      return;
+    }else{
+      this.page--;
+      this.listPlayers();
+    }
   }
+
+  irParaPagina(page: number){
+    this.page = page;
+    this.listPlayers();
+    this.firstLoad = false;
+  }
+
+  adjustStartPage(){
+    const totalPages = this.pages.length;
+    if (totalPages <= this.maxPagesToShow) {
+        this.startPage = 0;
+        return;
+    }
+    const triggerNextIndex = this.startPage + (this.maxPagesToShow - this.bufferPages);
+    
+    if (this.pageNumber >= triggerNextIndex) {
+      this.startPage = this.pageNumber - (this.maxPagesToShow - this.bufferPages);
+    } 
+    const bufferBack = this.bufferPages > 1 ? 1 : 0;
+    const triggerPrevIndex = this.startPage + bufferBack;
+    if (this.pageNumber < triggerPrevIndex) {
+      this.startPage = this.pageNumber - this.bufferPages + 1;
+    }
+    const maxPossibleStart = totalPages - this.maxPagesToShow;
+    if (this.startPage > maxPossibleStart) {
+      this.startPage = maxPossibleStart;
+    }
+    if (this.startPage < 0) {
+      this.startPage = 0;
+    }
+}
 
 }
